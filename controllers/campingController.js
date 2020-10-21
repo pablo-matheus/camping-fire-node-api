a03824e788d77b06848a7e542ac67249e700a44a
Pablo Matheus <pablo.lima.araujo@gmail.com>
2020-10-21 00:40:33 -0300

    implement camping model on controller to be able to do real database operations

--- controllers/campingController.js
+++ controllers/campingController.js
@@ -1,23 +1,46 @@
+const Camping = require('../models/campingModel');
+
 const campingController = {};
 
 campingController.listAll = (req, res) => {
-  res.status(200).send('Success!!!');
+  Camping.find()
+    .then((campings) => res.send(campings))
+    .catch(() => res.sendStatus(400));
 };
 
 campingController.getById = (req, res) => {
-  res.status(200).send('Success!!!');
+  const id = req.params.id;
+  Camping.findById(id)
+    .then((campings) => res.send(campings))
+    .catch(() => res.sendStatus(400));
 };
 
 campingController.submit = (req, res) => {
-  res.status(201).send('Success!!!');
+  const data = req.body;
+  if (!data) {
+    res.sendStatus(400);
+  }
+  new Camping(data).save()
+    .then(() => res.sendStatus(201))
+    .catch(() => res.sendStatus(400));
 };
 
 campingController.edit = (req, res) => {
-  res.status(200).send('Success!!!');
+  const id = req.params.id;
+  const data = req.body;
+  if(!id || !data) {
+    res.sendStatus(400);
+  }
+  Camping.findByIdAndUpdate(id, data)
+    .then(() => res.sendStatus(200))
+    .catch(() => res.sendStatus(400));
 };
 
 campingController.remove = (req, res) => {
-  res.status(200).send('Success!!!');
+  const id = req.params.id;
+  Camping.findByIdAndRemove(id)
+    .then(() => res.sendStatus(200))
+    .catch(() => res.sendStatus(400));
 };
 
 module.exports = campingController;
