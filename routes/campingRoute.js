f205637517fc737ad63b3dda9a053c1ee9794e27
Pablo Matheus <pablo.lima.araujo@gmail.com>
2020-10-21 00:31:01 -0300

    fixing camping endpoint

--- routes/campingRoute.js
+++ routes/campingRoute.js
@@ -5,8 +5,8 @@ const router = express.Router();
 
 router.get('/v1/campings', campingController.listAll);
 router.get('/v1/campings/:id', campingController.getById);
-router.post('v1/campings', campingController.submit);
-router.put('v1/campings/:id', campingController.edit);
-router.delete('v1/campings/:id', campingController.remove);
+router.post('/v1/campings', campingController.submit);
+router.put('/v1/campings/:id', campingController.edit);
+router.delete('/v1/campings/:id', campingController.remove);
 
 module.exports = router;
