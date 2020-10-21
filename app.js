1242ed7428b755f4a3f54318783b506355f49817
Pablo Matheus <pablo.lima.araujo@gmail.com>
2020-10-20 23:37:37 -0300

    changing mongoose error parameter name

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ mongoose.connect('mongodb://localhost:27017/camping_fire', {
   useNewUrlParser: true,
   useUnifiedTopology: true
 }).then(() => console.log('MongoDB Successful Connected!'))
-  .catch((err) => console.error('Failed to Connect to MongoDB ' + err));
+  .catch((error) => console.error('Failed to Connect to MongoDB ' + error));
 
 app.use(index);
 app.use(campingRoute);
