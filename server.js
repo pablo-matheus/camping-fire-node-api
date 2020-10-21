1a136119b38fe7da0d371ce006f8bbbdd4195c46
Pablo Matheus <pablo.lima.araujo@gmail.com>
2020-10-20 22:18:33 -0300

    separating route to new directory

--- server.js
+++ server.js
@@ -1,10 +1,6 @@
 const app = require('./app');
 const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
-
 app.listen(port, () => {
   console.log(`Server listening at port ${port}`);
 });
