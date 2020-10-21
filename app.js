6dc3cdbaa4b5f3a0577cdca1f4791d6b63de0027
Pablo Matheus <pablo.lima.araujo@gmail.com>
2020-10-21 00:34:15 -0300

    adding authentication to MongoDB connection string and adding json and urlencoded middlewares

--- app.js
+++ app.js
@@ -7,11 +7,14 @@ const campingRoute = require('./routes/campingRoute');
 const app = express();
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/camping_fire', {
+mongoose.connect('mongodb://root:aid3g7g21nklan@localhost:27017/camping_fire?authSource=admin', {
   useNewUrlParser: true,
   useUnifiedTopology: true
 }).then(() => console.log('MongoDB Successful Connected!'))
   .catch((error) => console.error('Failed to Connect to MongoDB ' + error));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.use(index);
 app.use(campingRoute);
