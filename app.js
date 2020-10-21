236bb51d61ae0e6110f5325aa7fff544bc20a6f9
Pablo Matheus <pablo.lima.araujo@gmail.com>
2020-10-20 23:29:46 -0300

    creating mongoose connection to MongoDB

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
 const index = require('./routes/index');
 const campingRoute = require('./routes/campingRoute');
 
 const app = express();
+
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost:27017/camping_fire', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}).then(() => console.log('MongoDB Successful Connected!'))
+  .catch((err) => console.error('Failed to Connect to MongoDB ' + err));
 
 app.use(index);
 app.use(campingRoute);
