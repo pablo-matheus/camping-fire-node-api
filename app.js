c594f3a1a6b51633fbc8cd3f2ca324cf931a188a
Pablo Matheus <pablo.lima.araujo@gmail.com>
2020-10-20 22:45:19 -0300

    using campingRoute

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const index = require('./routes/index');
+const campingRoute = require('./routes/campingRoute');
 
 const app = express();
 
 app.use(index);
+app.use(campingRoute);
 
 module.exports = app;
