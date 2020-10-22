2a062f2e03cb2e9dc1bb048a4b7ce25e03df2082
Pablo Matheus <pablo.lima.araujo@gmail.com>
2020-10-21 21:43:01 -0300

    implementing url parameters

--- controllers/campingController.js
+++ controllers/campingController.js
@@ -3,9 +3,28 @@ const Camping = require('../models/campingModel');
 const campingController = {};
 
 campingController.listAll = (req, res) => {
+  if (req.query.state && req.query.city) {
+    Camping.find({ 'location.state': req.query.state, 'location.city': req.query.city })
+      .then((campings) => res.send(campings))
+      .catch(() => res.sendStatus(400));
+    return console.log(`Finding by state [${req.query.state}] and city [${req.query.city}]`);
+  }
+  if (req.query.state) {
+    Camping.find({ 'location.state': req.query.state })
+      .then((campings) => res.send(campings))
+      .catch(() => res.sendStatus(400));
+    return console.log(`Finding by state [${req.query.state}]`);
+  }
+  if (req.query.city) {
+    Camping.find({ 'location.city': req.query.city })
+      .then((campings) => res.send(campings))
+      .catch(() => res.sendStatus(400));
+    return console.log(`Finding by city [${req.query.city}]`);
+  }
   Camping.find()
     .then((campings) => res.send(campings))
     .catch(() => res.sendStatus(400));
+  return console.log('Finding all');
 };
 
 campingController.getById = (req, res) => {
