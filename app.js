2ac2aee02bd6dad49ba5269814052a4f3ec39c42
Pablo Matheus <pablo.lima.araujo@gmail.com>
2020-10-21 23:22:50 -0300

    adding helmet to api

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const helmet = require('helmet');
 
 const index = require('./routes/index');
 const campingRoute = require('./routes/campingRoute');
@@ -15,6 +16,8 @@ mongoose.connect('mongodb://root:aid3g7g21nklan@localhost:27017/camping_fire?aut
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use(helmet());
 
 app.use(index);
 app.use(campingRoute);
